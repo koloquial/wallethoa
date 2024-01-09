9758870f84bedd141ab70f6ecf6da9d974bbceee
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2024-01-08 21:12:55 -0600

    updated fetch

--- src/requests/getAccount.js
+++ src/requests/getAccount.js
@@ -1,5 +1,9 @@
 export const getAccount = async (uid) => {
-    return await fetch(`${process.env.REACT_APP_MONGO_DB_URI}/users/${uid}`)
+    return await fetch(`${process.env.REACT_APP_MONGO_DB_URI}/users/${uid}`, {
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        })
         .then(res => res.json(res))
         .catch(err => console.log('err', err))
 }
